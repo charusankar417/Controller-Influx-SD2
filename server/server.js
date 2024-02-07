f725d4047bf4faf1c7000c70fe59bb2edde1a5f5
MunaimahK <munim2k@gmail.com>
2024-02-07 03:11:12 -0500

    server port updated

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
 app.use(cors({
-  origin: 'http://localhost:3002',
+  origin: 'http://localhost:3002' || 'http://localhost:3000',
   credentials: true
 }));
 
